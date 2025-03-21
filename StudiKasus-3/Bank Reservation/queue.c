50d6b275639f9a1ec3b6501f81087a40e3c07181
Dyooooou <juliandiokhusussekolah@gmail.com>
2025-03-22 00:39:24 +0700

    Update queue.c

--- StudiKasus-3/Bank Reservation/queue.c
+++ StudiKasus-3/Bank Reservation/queue.c
@@ -1,37 +1,58 @@
-#include "queue.h"
+
 #include <stdio.h>
-#include <stdlib.h>
+#include "queue.h"
 
-void CreateQueue(Queue *q) {
-    CreateList(q);
-}
-
-int IsQueueEmpty(Queue q) {
-    return (q.head == NULL);
+int CreateQueue(Queue *q) {
+    return CreateList(q);
 }
 
 void Enqueue(Queue *q, int value) {
     InsertLast(q, value);
 }
 
-int Dequeue(Queue *q) {
-    int value = -1;
-    if (!IsQueueEmpty(*q)) {
-        value = GetFirst(q);
-        DeleteFirst(q);
-    }
-    return value;
-}
-
-int QueueSize(Queue q) {
-    return CountNode(&q);
-}
-
-void PrintQueue(Queue q) {
+int Dequeue(Queue *q, int *value) {
     if (IsQueueEmpty(q)) {
-        printf("Queue kosong\n");
-    } else {
-        PrintForward(&q);
+        return 0;  
     }
+    *value = GetFirst(q);
+    
+    DeleteFirst(q);
+    
+    return 1; 
 }
 
+int IsQueueEmpty(Queue *q) {
+    return (q->head == NULL);
+}
+
+int QueueCount(Queue *q) {
+    return CountNode(q);
+}
+
+int QueueFront(Queue *q) {
+    if (IsQueueEmpty(q)) {
+        return -1;  
+    }
+    return GetFirst(q);
+}
+
+int QueueRear(Queue *q) {
+    if (IsQueueEmpty(q)) {
+        return -1; 
+    }
+    return GetLast(q);
+}
+
+void DisplayQueue(Queue *q) {
+    if (IsQueueEmpty(q)) {
+        printf("Antrian kosong\n");
+        return;
+    }
+    
+    printf("Antrian: ");
+    PrintForward(q);
+}
+
+void ClearQueue(Queue *q) {
+    ClearList(q);
+}
