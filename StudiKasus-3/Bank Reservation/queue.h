19f87e012ccc49c73d9dd75d1f771c5c5d2f3930
Dyooooou <juliandiokhusussekolah@gmail.com>
2025-03-22 00:42:57 +0700

    Update queue.h

--- StudiKasus-3/Bank Reservation/queue.h
+++ StudiKasus-3/Bank Reservation/queue.h
@@ -5,22 +5,14 @@
 
 typedef LinkedList Queue;
 
-// Create a new empty queue
-void CreateQueue(Queue *q);
-
-// Check if queue is empty
-int IsQueueEmpty(Queue q);
-
-// Add a new element to the queue (enqueue)
+int CreateQueue(Queue *q);
 void Enqueue(Queue *q, int value);
+int Dequeue(Queue *q, int *value);
+int IsQueueEmpty(Queue *q);
+int QueueCount(Queue *q);
+int QueueFront(Queue *q); // Function untuk dapatkan first Elmt
+int QueueRear(Queue *q);  // Function untuk dapatkan last Elmt
+void DisplayQueue(Queue *q);
+void ClearQueue(Queue *q);
 
-// Remove an element from the queue (dequeue)
-int Dequeue(Queue *q);
-
-// Get the number of elements in the queue
-int QueueSize(Queue q);
-
-// Display the queue
-void PrintQueue(Queue q);
-
-#endif // QUEUE_H
+#endif
